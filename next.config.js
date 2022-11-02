3fbdaeace2528b7fb006fc2b1a81c06c42334b67
Alex G Rice <alex@radiant.earth>
2022-11-02 11:57:49 -0600

    Add markdoc options: static mode.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withMarkdoc = require('@markdoc/next.js');
 const markdocOptions = { mode: 'static' };
 
 module.exports =
-  withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
+  withMarkdoc({ markdocOptions})({
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
   });
